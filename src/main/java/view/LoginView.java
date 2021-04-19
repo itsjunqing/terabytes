416780ea351b715209af44fbcdf27ed3e4f7837c
Nick <wmnicholasyeo@gmail.com>
2021-04-19 20:31:11 +0800

    Changed file names

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,9 +1,21 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginView extends JFrame {
-
-    private JPanel loginPanel;
+public class LoginView {
     private JPanel LoginView;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+    private JTextPane textPane1;
+
+    public LoginView() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+    }
 }
